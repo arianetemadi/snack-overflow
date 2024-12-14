79f0330675151932257fde2cb94eadd4f97b8b29
Arian ETEMADIHAGHIGHI <arian.etemadihaghighi@ist.ac.at>
2024-12-14 18:48:43 +0100

    Created data_util for functions regarding loading data

---
+++ src/data_util.py
@@ -0,0 +1,17 @@
+import conllu
+
+
+def load_data(path):
+    # load the conllu dataset
+    with open(path) as f:
+        data = conllu.parse(f.read())
+
+    # extract headlines (since a headline can have more than one sentence)
+    headlines = []
+    for i, sentence in enumerate(data):
+        if sentence.metadata["sent_id"] == "0":
+            headlines.append(data[i:i+1])
+        else:
+            headlines[-1].append(sentence)
+    
+    return headlines
