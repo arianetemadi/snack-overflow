7c1ca66ba51e93c4d64ec023c10e857d19b8932a
Arian ETEMADIHAGHIGHI <arian.etemadihaghighi@ist.ac.at>
2024-12-14 18:58:56 +0100

    Added setup.py, package can be installed now with 'pip install -e .'

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="snack-overflow",
+    version="0.0.1",
+    description="Project for Natural Language Processing and Information Extraction course, 2024WS",
+    license="MIT",
+    install_requires=[
+        "ipykernel==6.29.5",
+        "pandas==2.2.3",
+        "matplotlib==3.9.2",
+        "seaborn==0.13.2",
+        "conllu==6.0.0",
+        "nltk==3.9.1",
+        "wordfreq==3.1.1", 
+    ],
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+    ],
+    zip_safe=False,
+)
